579831cd0e9d9f916518781c473b644ea9973fdf
Leigh Johnson <hi@leighjohnson.me>
2021-11-11 22:54:32 -0800

    fix image filename

--- tools/manifest-to-env.sh
+++ tools/manifest-to-env.sh
@@ -6,10 +6,11 @@ OUT_DIR=env
 
 mkdir -p $OUT_DIR
 
+export IMAGE_FILENAME=$(cat dist/manifest.json | jq --raw-output '.builds[-1].custom_data.image_filename')
 export IMAGE_NAME=$(cat dist/manifest.json | jq --raw-output '.builds[-1].custom_data.image_name')
 export IMAGE_STAMP=$(cat dist/manifest.json | jq --raw-output '.builds[-1].custom_data.image_stamp')
 export IMAGE_PATH=$(cat dist/manifest.json | jq --raw-output '.builds[-1].custom_data.image_path')
-export IMAGE_URL=${CDN_BASE_URL}/${IMAGE_PATH}/${IMAGE_NAME}
+export IMAGE_URL=${CDN_BASE_URL}/${IMAGE_PATH}/${IMAGE_FILENAME}
 export CHECKSUM_URL=${CDN_BASE_URL}/${IMAGE_PATH}/sha256.checksum
 export MANIFEST_URL=${CDN_BASE_URL}/$IMAGE_PATH/manifest.json
 
