801472e506c528ca91f840c4d7f9840028c7bdac
Leigh Johnson <leigh@bitsy.ai>
2021-12-28 18:34:40 -0800

    add test + update nightly-release script

--- tools/nightly-release.sh
+++ tools/nightly-release.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-set +ue
+set -ue
 
-curl -X 'POST' \
-  'http://localhost:8000/api/releases/' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -H 'Authorization: Bearer ${PRINTNANNY_API_TOKEN}' \
-  -d '{
-  "name": "string",
-  "variant": "Desktop",
-  "image_url": "string",
-  "manifest_url": "string",
-  "sig_url": "string",
-  "checksum": "string",
-  "checksum_url": "string",
+
+BODY=$(cat <<EOF
+{
+  "name": "${IMAGE_FILENAME}",
+  "variant": "$IMAGE_VARIANT",
+  "image_url": "$IMAGE_URL",
+  "manifest_url": "$MANIFEST_URL",
+  "sig_url": "$SIG_URL",
+  "checksum": "$CHECKSUM",
+  "checksum_url": "$CHECKSUM_URL",
   "release_channel": "stable"
-}'
+}
+EOF
+)
+curl -X "POST" \
+  "${PRINTNANNY_API_URL}/api/releases/" \
+  -H "accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${PRINTNANNY_API_TOKEN}" \
+  -d "$BODY"
