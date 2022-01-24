f220f8d2dbf74376224a4104a47b0ea80d4ca074
Leigh Johnson <leigh@bitsy.ai>
2022-01-24 13:28:39 -0800

    perform full-upgrade on base image

--- tools/dist-upgrade.sh
+++ tools/dist-upgrade.sh
@@ -3,5 +3,4 @@
 set +eu
 
 DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y libfuse2
-DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade
