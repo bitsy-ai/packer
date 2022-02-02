c9265e954796fa4e30c4afd21b6db7483928789e
Leigh Johnson <leigh@bitsy.ai>
2022-02-01 19:04:40 -0800

    bake ansible collection version into image.txt (#55)

--- tools/image-version.sh
+++ tools/image-version.sh
@@ -5,3 +5,6 @@ set +eu
 DEST=/boot/image_version.txt
 echo "Writing $IMAGE_VERSION to $DEST"
 echo "$IMAGE_VERSION" > "$DEST"
+DEST=/boot/printnanny_os_version.txt
+PRINTNANNY_OS_VERSION=$(ansible-galaxy collection list bitsyai.rpi | grep bitsyai.rpi | cut -d" " -f2)
+echo "$PRINTNANNY_OS_VERSION" > "$DEST"
